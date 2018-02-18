d98f0389cda8ad0fc2bf40395fec0fd66874569a
Emmett Wester <33905560+aridmeme@users.noreply.github.com>
2018-02-17 20:04:01 -0700

    configure build.gradle.kts to build in jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
+buildscript {
+    repositories {
+        maven { setUrl("https://jitpack.io") }
+    }
+    dependencies {
+        classpath("com.github.hurricup:gradle-grammar-kit-plugin:2017.1.1")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.2.21" 
     idea
+    maven
 }
 
 repositories {
