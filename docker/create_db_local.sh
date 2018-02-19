7f492e5b5c4de4ccdd28aebf8abcf4867930fa37
Arid <33905560+aridmeme@users.noreply.github.com>
2018-02-19 13:42:39 -0700

    add script to set up database for local development using postgres in docker

---
+++ docker/create_db_local.sh
@@ -0,0 +1,28 @@
+#!/bin/sh 
+
+PG_USER=postgres
+PG_NAME=postgres
+
+sudo docker run -d --name $PG_NAME -p 5432:5432 $PG_USER
+sleep 2
+
+sudo docker exec -it $PG_NAME bash -c "psql -U $PG_USER -c 'CREATE DATABASE osrsxp;'"
+
+sudo docker exec -it $PG_NAME bash -c "psql -U $PG_USER osrsxp -c ' \
+    CREATE TABLE account( \
+        id SERIAL PRIMARY KEY, \
+        account_name VARCHAR(24), \
+        created_dt TIMESTAMP DEFAULT current_timestamp \
+    );'"
+
+sudo docker exec -it $PG_NAME bash -c "psql -U $PG_USER osrsxp -c '
+    CREATE TABLE skillxp( \
+        id SERIAL PRIMARY KEY, \
+        account_id INT REFERENCES account (id), \
+        name VARCHAR(24), \
+        rank BIGINT, \
+        level INT, \
+        xp BIGINT \
+    );'"
+
+
