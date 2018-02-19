7ef7bc03a5f6fb44c465b895dd2f9f93bf3dea12
Arid <33905560+aridmeme@users.noreply.github.com>
2018-02-19 14:01:25 -0700

    use osrsxp user instead of postgres user

--- docker/create_db_local.sh
+++ docker/create_db_local.sh
@@ -1,12 +1,13 @@
 #!/bin/sh 
 
-PG_USER=postgres
-PG_NAME=postgres
+PG_USER=osrsxp
+PG_NAME=osrsxp_pg
 
-sudo docker run -d --name $PG_NAME -p 5432:5432 $PG_USER
+sudo docker run -d --name $PG_NAME -p 5432:5432 postgres
 sleep 2
 
-sudo docker exec -it $PG_NAME bash -c "psql -U $PG_USER -c 'CREATE DATABASE osrsxp;'"
+sudo docker exec -it $PG_NAME bash -c "psql -U postgres -c 'CREATE DATABASE osrsxp;'"
+sudo docker exec -it $PG_NAME bash -c "psql -U postgres -c 'CREATE USER $PG_USER;'"
 
 sudo docker exec -it $PG_NAME bash -c "psql -U $PG_USER osrsxp -c ' \
     CREATE TABLE account( \
